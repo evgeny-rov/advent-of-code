d60dab49d014ba1835b9c74142c22353e3f75668
Evgeny Rodionov <bitsinmyhead@gmail.com>
2022-12-07 17:40:30 +0800

    added day7

---
+++ 2022/day7/a.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+fs.readFile('./input.txt', (_, data) => {
+  const lines = String(data).split(/\r?\n/);
+  const dirs = {};
+  const parserPath = [];
+
+  for (const line of lines) {
+    if (/\d+\s\w+/.test(line)) {
+      const fileSize = Number(line.match(/\d+/)[0]);
+
+      const path = [];
+
+      parserPath.forEach((dir) => {
+        path.push(dir);
+
+        const dirTotal = dirs[path.join('/')] ?? 0;
+        dirs[path.join('/')] = dirTotal + fileSize;
+      });
+    } else if (/\$ cd/.test(line)) {
+      const [_, _command, param] = line.split(' ');
+
+      param === '..' ? parserPath.pop() : parserPath.push(param);
+    }
+  }
+
+  const result = Object.values(dirs).reduce(
+    (total, dirSize) => (dirSize <= 100000 ? total + dirSize : total),
+    0
+  );
+
+  console.log(result);
+});
